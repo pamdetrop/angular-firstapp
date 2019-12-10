206c8b1b587d51c03c04ff40dc1ea760ed882936
pamdetrop <pamela.detroyat@gmail.com>
2019-12-10 19:30:53 +0100

    Ajout du formulaire pour le checkout

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { products } from '../products';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-product-details',
@@ -14,6 +15,7 @@ export class ProductDetailsComponent implements OnInit {
 
   constructor(
     private route: ActivatedRoute,
+    private cartService: CartService
   ) { }
 
   ngOnInit() {
@@ -22,4 +24,8 @@ export class ProductDetailsComponent implements OnInit {
     });
   }
 
+  addToCart(product) {
+    window.alert('Your product has been added to the cart!');
+    this.cartService.addToCart(product);
+  }
 }
